842bad0706b28b6b3ebb46891ccedf2e458cdb31
arkhipovds <arkhipovds@yandex.ru>
2019-09-26 23:43:05 +0300

    first steps

---
+++ src/components/appHeader.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-app>
+        <v-toolbar dark app class="primary">
+            <v-toolbar-title v-text="'Дежурства отедла СПДиТ'"></v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+            <v-btn v-for="(item,i) in menuItems" flat :key="`menuItem${i}`" :to="item.route">
+                <v-icon left v-html="item.icon"></v-icon>
+                {{item.title}} 
+            </v-btn>
+        </v-toolbar-items>
+        </v-toolbar>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        computed:{
+            menuItems(){
+                return [
+                    {
+                        icon: 'visibility',
+                        title: 'График',
+                        route: '/chart'
+                    },
+                    {
+                        icon: 'extension',
+                        title: 'Сотрудники',
+                        route: '/employees'
+                    },
+                    {
+                        icon: 'account_circle',
+                        title: 'Эффективность',
+                        route: '/efficiency'
+                    },
+                    {
+                        icon: 'exit_to_app',
+                        title: 'Выйти',
+                        route: '/logout'
+                    },
+                    {
+                        icon: 'input',
+                        title: 'Войти',
+                        route: '/login'
+                    }
+                ]
+            }
+        }        
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(vuetify);
 
 Vue.config.productionTip = false
 
