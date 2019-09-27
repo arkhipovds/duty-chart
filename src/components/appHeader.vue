4ab4b323fb161154be91cf5ffc5231063c49a6aa
arkhipovds <arkhipovds@yandex.ru>
2019-09-27 17:04:20 +0300

    continue

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,20 +1,38 @@
 <template>
-    <v-app>
+    <v-app-bar>
+        <v-navigation-drawer absolute temporary v-model="drawer">
+            <v-list>
+                <v-list-tile v-for="(item,i) in menuItems" :key="`drawerItem${i}`">
+                    <v-list-tile-action>
+                        <!-- <v-icon left>item.icon</v-icon> -->
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title v-text="item.title"></v-list-tile-title>
+                    </v-list-tile-content>
+                 </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
         <v-toolbar dark app class="primary">
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
             <v-toolbar-title v-text="'Дежурства отедла СПДиТ'"></v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-toolbar-items>
-            <v-btn v-for="(item,i) in menuItems" flat :key="`menuItem${i}`" :to="item.route">
-                <v-icon left v-html="item.icon"></v-icon>
-                {{item.title}} 
-            </v-btn>
-        </v-toolbar-items>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-btn v-for="(item,i) in menuItems" text :key="`menuItem${i}`" :to="item.route">
+        <!--          <v-icon left>item.icon</v-icon> -->
+                    {{item.title}} 
+                </v-btn>
+            </v-toolbar-items>
         </v-toolbar>
-    </v-app>
+    </v-app-bar>
 </template>
 
 <script>
     export default {
+        data(){
+            return{
+                drawer: false
+            }
+        },
         computed:{
             menuItems(){
                 return [
